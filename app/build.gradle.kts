57646306f30d3b3bcc05274dc3543583e754ae13
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2026-03-11 13:32:36 -0700

    bumping version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.tolstykh.eatABurrita"
         minSdk = 35
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
