bfeca4f89ada209b25485ccba315c0c4e9f84466
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2026-03-24 21:29:43 -0700

    bumping version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.tolstykh.eatABurrita"
         minSdk = 35
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         val gitCommit = providers.exec {
             commandLine("git", "rev-parse", "--short", "HEAD")
